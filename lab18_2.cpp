5cdf6cdacf3edfb73ade02140643f6f22661f22f
Atom <chanidapa.atom@gmail.com>
2026-02-10 20:36:11 +0700

    lab18_2.cpp completed

--- lab18_2.cpp
+++ lab18_2.cpp
@@ -5,8 +5,14 @@ struct Rect{
 	double x,y,w,h;
 };
 
-double overlap(){
+double overlap(Rect *R1, Rect *R2){
+ 	double left = max(R1->x, R2->x);
+    double right = min(R1->x + R1->w, R2->x + R2->w);
+    double top = min(R1->y, R2->y);
+    double bottom = max(R1->y - R1->h, R2->y - R2->h);
 
+    if (right <= left || top <= bottom)
+        return 0.0;
 
-
+    return (right - left) * (top - bottom);
 }
